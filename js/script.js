00a4da9416de7aa65b3e9eac369f68c678584394
maria&fonseca <maria.dsf.veleiros@gmail.com>
2025-04-30 16:00:55 -0300

    update graficos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-(function () { 
+(function () {
 
-"use strict";
+  "use strict";
   /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
 
   /**
    * Função de seleção
    */
-  
+
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,8 +20,8 @@
    * Easy event listener function 
    * Função de Adição de Eventos (on)
    * Função espera que um evento ocorra e depois responde a ele  */
-  
-  
+
+
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -34,30 +34,49 @@
   }
 
 
-// ativar botao menu no modo mobile 
+  // ativar botao menu no modo mobile 
 
-on('click', '.mobile-nav-toggle', function() {
+  on('click', '.mobile-nav-toggle', function () {
 
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
-})
-
-// efeito de digitação
-const typed=select('.typed');
-if(typed){
-  let typed_strings = typed.getAttribute('data-typed-items')
-  typed_strings = typed_strings.split(',')
-
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop:true,
-    typeSpeed: 100,//velocidade digitação
-  backSpeed: 50,//velocidade em apagar
-  backDelay: 1000,//tempo de espera
-  
   })
-}
-new PureCounter();
+
+  // efeito de digitação
+  const typed = select('.typed');
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,//velocidade digitação
+      backSpeed: 50,//velocidade em apagar
+      backDelay: 1000,//tempo de espera
+
+    })
+  }
+  new PureCounter();
+
+  /* Função waypoints */
+  //JS para skills = habilidades ou vendas //
+
+  let skilsContent = select('.skills-content');
+  if (skilsContent) { //verificar se o elemento existe//
+    new Waypoint({ //criar uma instancia //
+      element: skilsContent, 
+      offset: '80%', //janela de visualização//
+      handler:function(direction){
+        let progress =    select('.progress .progress-bar', true);
+        progress.forEach((el)=> {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+  })
+  }
+
+
 
 })()
